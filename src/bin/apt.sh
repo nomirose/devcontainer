1ba51c651c06b9ce71c73e7bf016939d989cfa2c
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-11-12 14:29:03 +0100

    feat: minor quality-of-life improvements (#7)
    Specifically:
    
    - Unminizmize (re-add packages for a system that users log into)
    - Switch default root shell
    - Disable VS Code first run notice

--- src/bin/apt.sh
+++ src/bin/apt.sh
@@ -6,3 +6,6 @@ export DEBIAN_FRONTEND
 apt-get update
 apt-get upgrade -y
 apt-get install -y --no-install-recommends build-essential
+
+# Re-add packages for a system that users log into
+yes | unminimize
---
+++ src/bin/system.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -ex
+
+# Switch default root shell
+sudo sed -i 's,/root:/bin/ash,/root:/bin/bash,' /etc/passwd
+
+# Disable VS Code first run notice
+config_dir='/home/vscode/.config/vscode-dev-containers'
+mkdir -p "${config_dir}"
+first_run_file='first-run-notice-already-displayed'
+touch "${config_dir}/${first_run_file}"
