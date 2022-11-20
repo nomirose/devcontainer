33242247b9e0ab919c11d317120b50de7d8bce57
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-11-20 22:12:58 +0100

    refactor: add `custom_umminmize` function (#10)

--- src/bin/system.sh
+++ src/bin/system.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 # Switch default root shell
-sudo sed -i 's,/root:/bin/ash,/root:/bin/bash,' /etc/passwd
+sed -i 's,/root:/bin/ash,/root:/bin/bash,' /etc/passwd
 
 # Disable VS Code first run notice
 config_dir='/home/vscode/.config/vscode-dev-containers'
