94659c0c4838cf05b892e66444bbaa833cda73fc
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-08 17:34:11 +0100

    feat: Switch to Debian with the latest Node.js (#12)
    Additionally:
    
    - Disable unminimizing because our modifications were specific to the
      Ubuntu version of that program. We will have to experiment with the
      Debian version before we know what to do with the commented out code.

--- src/bin/homebrew.sh
+++ src/bin/homebrew.sh
@@ -2,6 +2,9 @@
 
 HOMEBREW='https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'
 
+# Signal to the Homebrew installer that we can run as root
+touch /.dockerenv
+
 set +x
 /bin/bash -c "$(curl -fsSL ${HOMEBREW})"
 set -x
@@ -9,5 +12,5 @@ set -x
 brew install \
     gcc \
     git \
-    hadolint \
+    gh \
     ltex-ls
