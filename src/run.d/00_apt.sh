7f56ff1acda418e881740e43cf9e5170bb7df1ee
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-09 18:49:53 +0100

    fix: Switch to using run.d scripts (#26)
    Additionally:
    
    - Move APT setup code to `00_apt.sh`, leaving `01_npm.sh` for NPM setup
    - Install `trunk` and `vale`

--- src/run.d/00_apt.sh
+++ src/run.d/00_apt.sh
@@ -18,6 +18,20 @@ notify() {
     printf '\n\e[1m%s\e[0m\n\n' "${message}"
 }
 
+# Custom sources
+# -----------------------------------------------------------------------------
+
+# Set up APT for latest Node.js
+curl -fsSL https://deb.nodesource.com/setup_19.x | bash -
+
+# Set up APT for latest Yarn
+YARN_KEY=/usr/share/keyrings/yarnkey.gpg
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg |
+  gpg --dearmor |
+  tee "${YARN_KEY}" >/dev/null
+echo "deb [signed-by=${YARN_KEY}] https://dl.yarnpkg.com/debian stable main" |
+  tee /etc/apt/sources.list.d/yarn.list
+
 # Unminimize
 # -----------------------------------------------------------------------------
 
