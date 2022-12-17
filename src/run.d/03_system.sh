698a1bc8e14eca5bf49d7857d0a9722a8d9b3207
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-17 20:51:57 +0100

    feat: Integrate direnv into the shell

--- src/run.d/03_system.sh
+++ src/run.d/03_system.sh
@@ -4,7 +4,13 @@
 sed -i 's,/root:/bin/ash,/root:/bin/bash,' /etc/passwd
 
 # Disable VS Code first run notice
-config_dir='/home/vscode/.config/vscode-dev-containers'
+config_dir='/root/.config/vscode-dev-containers'
 mkdir -p "${config_dir}"
 first_run_file='first-run-notice-already-displayed'
 touch "${config_dir}/${first_run_file}"
+
+# Set up direnv
+cat >>/root/.bashrc <<EOF
+
+"\$(direnv export bash)"
+EOF
