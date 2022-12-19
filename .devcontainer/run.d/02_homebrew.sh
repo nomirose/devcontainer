536699a031f79519d9d3d797469e95f6f3eff203
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-19 23:25:32 +0000

    fix: update cSpell config and address lint issues
    - Convert `.cspell.yaml` to `.cspell.json` because cSpell ignores the
      former
    - Add more dictionaries to `.cspell.json`
    - Install the cSpell CLI tool (to trace dictionaries)
    - Add `vocab.txt` file for custom words
    - Fix lint issues (mostly shellcheck)

--- .devcontainer/run.d/02_homebrew.sh
+++ .devcontainer/run.d/02_homebrew.sh
@@ -1,13 +1,20 @@
 #!/bin/sh -e
 
-HOMEBREW='https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'
+GH_RAW_URL=https://raw.githubusercontent.com
+INSTALL_SCRIPT=install.sh
+INSTALL_URL="${GH_RAW_URL}/Homebrew/install/HEAD/${INSTALL_SCRIPT}"
 
 # Signal to the Homebrew installer that we can run as root
 touch /.dockerenv
 
-set +x
-/bin/bash -c "$(curl -fsSL ${HOMEBREW})"
-set -x
+tmp_dir="$(mktemp -d)"
+clean() { rm -rf "${tmp_dir}"; }
+trap clean EXIT
+
+cd "${tmp_dir}"
+wget -q "${INSTALL_URL}" -O "${INSTALL_SCRIPT}"
+chmod 755 "${INSTALL_SCRIPT}"
+NONINTERACTIVE=false "./${INSTALL_SCRIPT}"
 
 brew install \
     direnv \
